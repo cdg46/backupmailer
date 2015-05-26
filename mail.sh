03fb19bc8013c97f6bc9ed4cb5df3f5070236e3a
root <root@sauv-coll.ip-37-187-252.eu>
2015-05-26 16:01:22 +0200

    + preparation flag x-priority

--- mail.sh
+++ mail.sh
@@ -7,9 +7,9 @@ else
   source $(dirname $0)/config.sh.dist
 fi
 
-##############################################"
 while read userid homedir email last_login
 do
+  ERRORCOLL=0
   RAPPORTCOLL=$BASE/backupreport_$userid
   echo "Bonjour,
 
@@ -23,6 +23,7 @@ Veuillez trouver un rapport de votre sauvegarde a distance sur les serveurs du C
 
     if [ "$last_login" == "0000-00-00 00:00:00" ]
     then
+      ERRORNEVERLOGGED=1
       echo "  * ${userid}" >> $NEVERLOGGED
       printf "%s %s %s %s _____-__-__ __:__\n" $userid "${col1:${#userid}}" $SIZE "${col2:${#SIZE}}" >> $RAPPORT
     else
@@ -36,6 +37,8 @@ Veuillez trouver un rapport de votre sauvegarde a distance sur les serveurs du C
       ## Est-ce que la derniere connexion remonte a plus d'une semaine ?
       if [ "$DIFF" -ge 604800 ]
       then
+        ERRORCOLL=1
+        ERRORTOOLD=1
         echo "/!\ cela fait plus d'une semaine qu'aucun fichier n'a ete depose sur le serveur
 " >> $RAPPORTCOLL
         printf "%s %s $last_login\n" $userid "${col1:${#userid}}"  >> $TOOLD
@@ -53,6 +56,8 @@ Votre derniere sauvegarde sur ce serveur date du ${last_login}" >> $RAPPORTCOLL
     fi
   else
     if [ "$userid" != "userid" ]; then
+      ERRORCOLL=1
+      ERRORNOHOMEDIR=1
       echo "  * ${userid}" >> $NOHOMEDIR
       SIZE=N/A
       printf "%s %s %s\n" $userid "${col1:${#userid}}" $SIZE >> $RAPPORT
@@ -67,13 +72,39 @@ Votre derniere sauvegarde sur ce serveur date du ${last_login}" >> $RAPPORTCOLL
 Service de sauvegarde a distance du CDG46
 
 email : sauvegarde@cdgfpt46.fr
-tel : 05 32 28 00 15 " >> $RAPPORTCOLL
+tel : 05 32 28 00 15" >> $RAPPORTCOLL
 
-    cat $RAPPORTCOLL | mail -s "[CDG46][Sauvegarde][Rapport hebdomadaire]" $email
+    # Il y a un souci donc envoi specifique
+    if [ ! -z "$ERRORCOL" ]
+    then
+      echo "
+#################################################
+#                                               #
+#  VEUILLEZ CONTACTER LE SERVICE DE SAUVEGARDE  #
+#                                               #
+#          PAR TELEPHONE OU PAR MAIL            #
+#                                               #
+#################################################
+" >> $RAPPORTCOLL
+
+      cat $RAPPORTCOLL | mail -S "X-Priority: 1" -S "Importance: high" -s "[CDG46][Sauvegarde][Rapport hebdomadaire]" $email
+    else
+      cat $RAPPORTCOLL | mail -s "[CDG46][Sauvegarde][Rapport hebdomadaire]" $email
+    fi
   fi
 done < <(echo "SELECT userid, homedir, email, last_login FROM users ORDER BY userid ASC" | mysql $DB -u $DBUSER -p$DBPWD)
 
-cat $RAPPORT     | mail -s "[CDG46][Sauvegarde][Etat du jour]"       $admin
-cat $TOOLD       | mail -s "[CDG46][Sauvegarde][Plus d'une semaine]" $admin
-cat $NEVERLOGGED | mail -s "[CDG46][Sauvegarde][Jamais connecte]"    $admin
-cat $NOHOMEDIR   | mail -s "[CDG46][Sauvegarde][Repertoire vide]"    $admin
+cat $RAPPORT | mail -s "[CDG46][Sauvegarde][Etat du jour]" $admin
+
+if [ ! -z "$ERRORNOHOMEDIR" ]
+then
+  cat $NOHOMEDIR | mail -S "X-Priority: 1" -S "Importance: high" -s "[CDG46][Sauvegarde][Repertoire vide]" $admin
+fi
+if [ ! -z "$ERRORTOOLD" ]
+then
+  cat $TOOLD | mail -S "X-Priority: 1" -S "Importance: high" -s "[CDG46][Sauvegarde][Plus d'une semaine]" $admin
+fi
+if [ ! -z "$ERRORNEVERLOGGED" ]
+then
+  cat $NEVERLOGGED | mail -S "X-Priority: 1" -S "Importance: high" -s "[CDG46][Sauvegarde][Jamais connecte]" $admin
+fi
